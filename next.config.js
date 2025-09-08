12f575e773519373386c35dffc272e82ffc26abe
falakhe <fsivela@fragomen.com>
2025-09-08 20:42:17 +0200

    fix: build error

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  // Avoid SWC minification bug that can corrupt `return true` in production
+  swcMinify: false,
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
